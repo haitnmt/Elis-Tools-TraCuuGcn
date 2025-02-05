b4f1873233e1099e71ec1e61c978e45a9bf51ef7
haitnmt <haitnmt@gmail.com>
2025-02-05 16:49:24 +0700

    Add AppSettings extension and service for configuration management

---
+++ src/Haihv.Elis.Tool.TraCuuGcn.Web-App/Haihv.Elis.Tool.TraCuuGcn.Web-App.Client/Extensions/AppSettingsExtension.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Haihv.Elis.Tool.TraCuuGcn.WebLib.Extensions;
+
+public static class AppSettingsExtension
+{
+    public static void AddAppSettingsServices(this IServiceCollection services)
+    {
+        
+    }
+}
+
+public sealed class AppSettingsServices(string? baseUrl = null)
+{
+    private readonly HttpClient _httpClient = new HttpClient(
+    {
+        BaseAddress = new Uri(baseUrl)
+    });
+    public string? ApiEndpoint { get; set; }
+    public  bool IsTrialVersion { get; set; }
+}
---
+++ src/Haihv.Elis.Tool.TraCuuGcn.WebLib/Services/AppSettingsService.cs
@@ -0,0 +1,35 @@
+﻿using System.Net.Http.Json;
+using System.Reflection;
+
+namespace Haihv.Elis.Tool.TraCuuGcn.WebLib.Services;
+
+public sealed class AppSettingsServices(string baseUrl)
+{
+    private readonly HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri(baseUrl)
+    };
+
+    public AppSettings AppSettings => GetAppSettings().Result;
+    
+    private async Task<AppSettings> GetAppSettings()
+    {
+        var appSettings =  await _httpClient.GetFromJsonAsync<AppSettings>("/api/appsettings");
+        if (appSettings == null)
+            return new AppSettings
+            {
+                AppVersion = Assembly.GetExecutingAssembly().GetName().Version!.ToString(),
+                IsTrialVersion = true
+            };
+        return appSettings;
+    }
+}
+
+public sealed class AppSettings
+{
+    public string? ApiEndpoint { get; set; }
+    public string? ApiVersion { get; set; }
+    public string? AppVersion { get; set; }
+    public  bool IsTrialVersion { get; set; }
+    
+}
