131b7f20a53f2eddcc10d8c2d80adacecab511dd
haitnmt <haitnmt@gmail.com>
2025-04-24 15:28:30 +0700

    Xóa cấu hình CI/CD và toàn bộ code liên quan đến giao diện KeyCloak (CSS, JS, và file ngôn ngữ).

--- src/AppHost/Program.cs
+++ src/AppHost/Program.cs
@@ -2,8 +2,12 @@ var builder = DistributedApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
 var cacheName = configuration["Aspire:CacheName"] ??  "cache";
 var cache  = builder.AddValkey(cacheName,  6379);
+var sdeApi = builder.AddGolangApp("sdeApi", "../Haihv.Elis.Tool.TraCuuGcn.Api.Sde")
+    .WithHttpEndpoint(8151, 8151, "localhost", isProxied: false);
 var api = builder.AddProject<Projects.Haihv_Elis_Tool_TraCuuGcn_Api>("TraCuuGcn-api")
+    .WithReference(sdeApi)
     .WithReference(cache)
+    .WaitFor(sdeApi)
     .WaitFor(cache);
 builder.AddProject<Projects.Haihv_Elis_Tool_TraCuuGcn_Web_App>("TraCuuGcn-app")
     .WithReference(cache)
